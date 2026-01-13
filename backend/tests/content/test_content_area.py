292782093782702a5f461ed48e39ef4631d0f38b
Rhafael Freitas da Costa <costa.cfr@gmail.com>
2026-01-13 14:53:48 -0300

    Add arquivo de teste

--- backend/tests/content/test_content_area.py
+++ backend/tests/content/test_content_area.py
@@ -1,3 +1,4 @@
+from AccessControl import Unauthorized
 from lactec.intranet.content.area import Area
 from plone import api
 from plone.dexterity.fti import DexterityFTI
@@ -58,3 +59,24 @@ class TestArea:
             content = api.content.create(container=self.portal, **area_payload)
         assert content.portal_type == CONTENT_TYPE
         assert isinstance(content, Area)
+
+    @pytest.mark.parametrize(
+        "role,allowed",
+        [
+            ["Manager", True],
+            ["Site Administrator", True],
+            ["Editor", False],
+            ["Reviewer", False],
+            ["Contributor", False],
+            ["Reader", False],
+        ],
+    )
+    def test_create_with_allowed(self, area_payload, role: str, allowed: bool):
+        with api.env.adopt_roles([role]):
+            if allowed:
+                content = api.content.create(container=self.portal, **area_payload)
+                assert content.portal_type == CONTENT_TYPE
+                assert isinstance(content, Area)
+            else:
+                with pytest.raises(Unauthorized):
+                    api.content.create(container=self.portal, **area_payload)
