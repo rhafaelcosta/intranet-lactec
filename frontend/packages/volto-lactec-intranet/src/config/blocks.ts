61a60246c378c5ffc5ac3460a6434a40acf6b6e1
Rhafael Freitas da Costa <costa.cfr@gmail.com>
2026-01-21 14:37:32 -0300

    Exercício 46: Adiciona bloco de previsão do clima

--- frontend/packages/volto-lactec-intranet/src/config/blocks.ts
+++ frontend/packages/volto-lactec-intranet/src/config/blocks.ts
@@ -1,7 +1,25 @@
 import type { ConfigType } from '@plone/registry';
+import type { BlockConfigBase } from '@plone/types';
 import AreaGridItem from 'volto-lactec-intranet/components/Blocks/Grid/AreaGridItem';
+import ClimaBlockInfo from 'volto-lactec-intranet/components/Blocks/Clima';
+
+declare module '@plone/types' {
+  // Altera a interface BlocksConfigData para incluir o bloco de Clima
+  export interface BlocksConfigData {
+    climaBlock: BlockConfigBase;
+  }
+}
+
+function installLocalBlocks(config: ConfigType) {
+  // Instala o bloco de Clima
+  config.blocks.blocksConfig.climaBlock = ClimaBlockInfo;
+  return config;
+}
 
 export default function install(config: ConfigType) {
+  // Registra todos os blocos criados nesse pacote
+  installLocalBlocks(config);
+
   // Registra Componente para exibir uma Área quando a listagem for de Grade
   config.registerComponent({
     name: 'GridListingItemTemplate',
